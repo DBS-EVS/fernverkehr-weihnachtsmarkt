bc88a307a50ef46cd79656114153e7c97d8b9e63
DBS-EVS <104421792+DBS-EVS@users.noreply.github.com>
2022-11-09 14:38:25 +0000

    Deploying to gh-pages from master @ 1c54455e4d8a4a8fe9976ebd9cc20515c8e46176 🚀

--- assets/skript_xmas.d956630f.js
+++ assets/skript_xmas.d956630f.js
@@ -4,6 +4,6 @@ Oben Rechts: IT & Regelwerk
 
 Unten Rechts: Rettungsz\xFCge
 
-Unten Links:ZKB, FeB, Fahrzeugeinsatz
+Unten Links: ZKB, FeB, Fahrzeugeinsatz
 
 Oben Links: Auswertung und Abschluss`,[{label:"Schlie\xDFen",callback:o=>{e=!0,a()}}])});WA.room.onLeaveZone("directions1",()=>{a(),e&&(WA.nav.closeCoWebSite(),e=!1)});WA.room.onEnterZone(t,()=>{n=WA.ui.openPopup("popUpMusicModern","Musik?",[{label:"Her damit!",callback:o=>{WA.nav.openCoWebSite(p),e=!0,a()}}])});WA.room.onLeaveZone(t,()=>{a(),e&&(WA.nav.closeCoWebSite(),e=!1)});WA.room.onEnterZone(i,()=>{n=WA.ui.openPopup("popUpDirections2","M\xF6chtest du die Aufgabe starten?",[{label:"KIRA Entwicklung",callback:o=>{WA.nav.openTab(WA.state.it_1)}},{label:"PP-FZD Tipps",callback:o=>{WA.nav.openTab(WA.state.it_2)}},{label:"IMS",callback:o=>{WA.nav.openTab(WA.state.it_3)}},{label:"Z\xFCge zur\xFCckhalten",callback:o=>{WA.nav.openTab(WA.state.it_4)}}])});WA.room.onLeaveZone(i,()=>{a()});WA.room.onEnterZone("game_fire",()=>{WA.nav.openCoWebSite(WA.state.game_classic),e=!0});WA.room.onLeaveZone("game_fire",()=>{e&&(WA.nav.closeCoWebSite(),e=!1)});
