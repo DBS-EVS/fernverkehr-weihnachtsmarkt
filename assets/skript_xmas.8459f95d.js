e69a89fc8cc5e52557c8ecd65ff866dbffdd8814
DBS-EVS <104421792+DBS-EVS@users.noreply.github.com>
2022-11-09 16:19:48 +0000

    Deploying to gh-pages from master @ db04c709ce685360fd0c2ce6574b9dba477f6217 🚀

---
+++ assets/skript_xmas.8459f95d.js
@@ -0,0 +1,6 @@
+import"https://unpkg.com/@workadventure/scripting-api-extra@^1";var n=!1,e=void 0,i="https://www.youtube.com/embed/nHxsomFVJtU";function a(){e!==void 0&&(e.close(),e=void 0)}var t="music_modern",p="directions2";WA.room.onEnterZone("directions1",()=>{e=WA.ui.openPopup("popUpStart",`Willkommen! Du findest folgende Bereiche:
+
+Oben Rechts: IT & Regelwerk
+Unten Rechts: Rettungsz\xFCge
+Unten Links: ZKB, FeB, Fahrzeugeinsatz
+Oben Links: Auswertung und Abschluss`,[{label:"Schlie\xDFen",callback:o=>{a()}}])});WA.room.onLeaveZone("directions1",()=>{a()});WA.room.onEnterZone(t,()=>{e=WA.ui.openPopup("popUpMusicModern","Musik?",[{label:"Her damit!",callback:o=>{WA.nav.openCoWebSite(i),n=!0,a()}}])});WA.room.onLeaveZone(t,()=>{a(),n&&(WA.nav.closeCoWebSite(),n=!1)});WA.room.onEnterZone(p,()=>{e=WA.ui.openPopup("popUpDirections2","M\xF6chtest du die Aufgabe starten?",[{label:"KIRA Entwicklung",callback:o=>{WA.nav.openTab(WA.state.it_1)}},{label:"PP-FZD Tipps",callback:o=>{WA.nav.openTab(WA.state.it_2)}},{label:"IMS",callback:o=>{WA.nav.openTab(WA.state.it_3)}},{label:"Z\xFCge zur\xFCckhalten",callback:o=>{WA.nav.openTab(WA.state.it_4)}}])});WA.room.onLeaveZone(p,()=>{a()});WA.room.onEnterZone("game_fire",()=>{WA.nav.openCoWebSite(WA.state.game_classic),n=!0});WA.room.onLeaveZone("game_fire",()=>{n&&(WA.nav.closeCoWebSite(),n=!1)});
